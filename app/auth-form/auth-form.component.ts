1babb3db26406c7aed3384070b4fcc603e59b735
extrahappyperson <ht.uzpharma@gmail.com>
2019-07-01 14:25:01 +0200

    add component html

--- app/auth-form/auth-form.component.ts
+++ app/auth-form/auth-form.component.ts
@@ -4,24 +4,7 @@ import { User } from './auth-form.interface';
 
 @Component({
   selector: 'auth-form',
-  template: `
-    <div>
-      <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
-        <ng-content></ng-content>
-        <label>
-          Email address
-          <input type="email" name="email" ngModel>
-        </label>
-        <label>
-          Password
-          <input type="password" name="password" ngModel>
-        </label>
-        <button type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
-  `
+  templateUrl:'./auth-form.component.html'
 })
 export class AuthFormComponent {
 
